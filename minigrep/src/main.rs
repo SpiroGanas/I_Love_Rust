9cf9ef4bacbec3c7a7b7f9077c7fd62a2c5a9826
Spiro Ganas <spiroganas@gmail.com>
2024-09-11 11:33:05 -0400

    Update main.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,8 @@
+// Spiro Ganas
+// 4/8/2024
+//
+// minigrep program from "The Rust Programming Language" online book:  https://doc.rust-lang.org/book/ch12-00-an-io-project.html
+
 use std::env;
 
 fn main() {
